baff37c0d847f44616cc68bcbde573eea636c8e6
Victor A <victor@namespace.ee>
2020-03-30 15:58:50 +0300

    update data-package json

---
+++ client/build/precache-manifest.fd4bdd32a90483aef3b4757fb9f7d245.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1fbf9178c5776779681b35f4c84f593",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f45bb7a369e6929df037",
+    "url": "/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "7e3bd9684df88ac57ffe",
+    "url": "/static/css/main.7aeb6fbc.chunk.css"
+  },
+  {
+    "revision": "f45bb7a369e6929df037",
+    "url": "/static/js/2.515d6319.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.515d6319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e3bd9684df88ac57ffe",
+    "url": "/static/js/main.3b803f80.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -11,14 +11,14 @@
  * See https://goo.gl/2aRDsh
  */
 
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+  "/precache-manifest.fd4bdd32a90483aef3b4757fb9f7d245.js"
 );
 
-importScripts("/precache-manifest.cef24df936340ca7ea92955e6ec37fc3.js");
-
-self.addEventListener("message", event => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
@@ -33,9 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL("/index.html"),
-  {
-    blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/]
-  }
-);
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
